a1cb9568e8534c0070fb1df4c5d82cc905a431e8
shafayetShafee <sshafee@isrt.ac.bd>
2024-08-10 18:09:30 +0600

    feat: updated pkg namespace

--- src/skmiscpy/__init__.py
+++ src/skmiscpy/__init__.py
@@ -1,3 +1,8 @@
-# read version from installed package
 from importlib.metadata import version
+
 __version__ = version("skmiscpy")
+
+from skmiscpy.here import here
+from skmiscpy.smd import compute_smd
+from skmiscpy.plot_smd import plot_smd
+from skmiscpy.plot_mirror_histogram import plot_mirror_histogram

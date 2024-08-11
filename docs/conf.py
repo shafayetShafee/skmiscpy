ab53316baab9c6196183a658ae37fb64d99b5bbc
shafayetShafee <sshafee@isrt.ac.bd>
2024-08-12 02:26:53 +0600

    build: added customization for theme

--- docs/conf.py
+++ docs/conf.py
@@ -39,5 +39,82 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+html_show_sphinx = False
 
 html_theme = "sphinx_immaterial"
+
+
+# material theme options (see theme.conf for more information)
+html_theme_options = {
+    "icon": {
+        "repo": "fontawesome/brands/github",
+        "edit": "material/file-edit-outline",
+    },
+    "site_url": "https://skmiscpy.readthedocs.io",
+    "repo_url": "https://github.com/shafayetShafee/skmiscpy",
+    "repo_name": "skmiscpy",
+    "edit_uri": "blob/main/docs",
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+        "content.tabs.link",
+        "announce.dismiss",
+    ],
+    "palette": [
+        {
+            "media": "(prefers-color-scheme: light)",
+            "scheme": "default",
+            "primary": "indigo",
+            "accent": "light-green",
+            "toggle": {
+                "icon": "material/weather-night",
+                "name": "Switch to dark mode",
+            },
+        },
+        {
+            "media": "(prefers-color-scheme: dark)",
+            "scheme": "slate",
+            "primary": "deep-orange",
+            "accent": "lime",
+            "toggle": {
+                "icon": "material/weather-sunny",
+                "name": "Switch to light mode",
+            },
+        },
+    ],
+    # BEGIN: version_dropdown
+    "version_dropdown": True,
+    "version_info": [
+        {
+            "version": "https://skmiscpy.readthedocs.io",
+            "title": "ReadTheDocs",
+            "aliases": [],
+        },
+    ],
+    # END: version_dropdown
+    "toc_title_is_page_title": True,
+    # BEGIN: social icons
+    "social": [
+        {
+            "icon": "fontawesome/brands/github",
+            "link": "https://github.com/shafayetShafee/skmiscpy",
+            "name": "Source on github.com",
+        },
+        # {
+        #     "icon": "fontawesome/brands/python",
+        #     "link": "https://pypi.org/project/sphinx-immaterial/",
+        # },
+    ],
+    # END: social icons
+}
